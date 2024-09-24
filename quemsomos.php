a6bf27f8ec8b456832ad4d900228b9a40dcb2a33
Ellen Ruth <167252820+Gh0St-Nimos@users.noreply.github.com>
2024-09-24 08:40:35 -0300

    Update quemsomos.php

--- quemsomos.php
+++ quemsomos.php
@@ -2,9 +2,11 @@
     include_once 'topo.php';
     include_once 'menu.php';
 ?>
-<section>
+<section texto-centralizado>
     <h2>Quem Somos</h2>
-    <p>Somos uma empresa dedicada a oferecer as melhores soluções.</p>
+    <p>Muito tempo atrás, duas raças dominavam a Terra: Humanos e Monstros. Um dia, a Guerra começou entre as duas raças. Após uma longa série de batalhas, os Humanos saíram vitoriosos. O maior de seus Magos prendeu os monstros no Subterrâneo, com uma Barreira mágica. Entretanto, essa barreira não era perfeita.Muitos anos após a guerra, em 201X, a Criança Humana escala a Montanha Ebott por um motivo desconhecido. Dizem que aqueles que sobem a montanha nunca retornam. Lá, a criança encontra um enorme buraco e nele cai enquanto tentava ver mais de perto.</p>
+
+    <p>O Protagonista, que deve ser nomeado pelo jogador, começa sua aventura nas Ruínas. Rapidamente, encontra Flowey, a flor animada que tenta matar o protagonista por diversão. Também encontra Toriel, um monstro materno, que o salva e o ensina a resolver quebra-cabeças e conflitos no Subterrâneo. Após enfrentá-la, ela permite que a Criança saia das Ruínas.</p>
 </section>
 <?php
     include_once 'rodape.php';
