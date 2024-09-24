ae743ffa5f49c3a5b9208acf36d7f5caa98255ab
Ellen Ruth <167252820+Gh0St-Nimos@users.noreply.github.com>
2024-09-24 09:29:22 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <html>
 <head>
     <link rel="icon" href="imagens/coracion.png" type="image/png">
+    <!-- js aqui -->
 </head>
-</html>
 <?php
     include_once('topo.php');
     include_once('menu.php');
-?>
-<section>
-    <h2>Welcome to the Underground!</h2>
-    <p>:)</p>
-</section>
 
-<?php
+    if(empty($_SERVER["QUERY_STRING"])){
+        $var = "principal.php";
+        include_once("$var");
+    }else{
+        $pg = $_GET['pg'];
+        include_once("$pg.php");
+    }
+
     include_once('rodape.php');
 ?>
+</html>
